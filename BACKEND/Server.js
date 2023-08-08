2dbcbfb4487e5c4787c4061b26fcbccabc70b7ab
IT21258480 <100502665+Pathumdissanayake@users.noreply.github.com>
2023-08-08 19:23:59 +0530

    Backend configurations

--- BACKEND/Server.js
+++ BACKEND/Server.js
@@ -1,11 +1,25 @@
 const express = require ('express');
 const mongoose = require ('mongoose');
-const bodyParser = requre ('body-parser');
+const bodyParser = require ('body-parser');
 const cors = require ('cors');
-const dotenv = require ('dotenv');
 const App = express();
+require ("dotenv").config();
 
-const PORT = process.env.PORT || 8070;
+const PORT = process.env.PORT || 4000;
 
 App.use(cors());
 App.use(bodyParser.json());
+
+//connect to mongo db
+const URL = process.env.MONGODB_URL;
+mongoose.connect(URL)
+.then(() => {
+
+    // listen to port
+    App.listen(PORT, () => {
+      console.log(`Connected to db & listening for requests on port ${PORT}`)
+    })
+  })
+  .catch((err) => {
+    console.log(err)
+  }) 
