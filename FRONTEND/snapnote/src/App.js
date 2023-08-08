252fcc985ce8bef2b0987e9b82f7d2ff5c35534a
IT21258480 <100502665+Pathumdissanayake@users.noreply.github.com>
2023-08-08 21:15:38 +0530

    Update App.js

--- FRONTEND/snapnote/src/App.js
+++ FRONTEND/snapnote/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './Components/HomePage';
+import AddForm from './Components/AddForm';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="Home" element={<HomePage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/New-note" element={<AddForm />} />
       </Routes>
     </BrowserRouter>
   );
