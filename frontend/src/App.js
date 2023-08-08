307bdc6d576f95938314570a72f881ecc71b84cc
IT21258480 <100502665+Pathumdissanayake@users.noreply.github.com>
2023-08-08 16:53:10 +0530

    Added Frontend Components

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './Components/HomePage';
+import AddForm from './Components/AddForm';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/New-note" element={<AddForm/>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
