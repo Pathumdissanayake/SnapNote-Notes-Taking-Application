591bded9b1e34352f81079ad2e21063869b7e22e
IT21258480 <100502665+Pathumdissanayake@users.noreply.github.com>
2023-08-09 10:31:12 +0530

    Backend models added

--- BACKEND/MODELS/NotesModel.js
+++ BACKEND/MODELS/NotesModel.js
@@ -1,16 +1,16 @@
-const mongoose = require ("mongoose");
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const NotesSchema = new Schema ({
-    Title : {
-        type : String,
-        requred : true //validating title field
-    },
-    Content : {
-        type : String,
-        requred : true //validating content field
-    }
-})
+const NotesSchema = new Schema({
+  title: {
+    type: String,
+    requred: true, //validating title field
+  },
+  content: {
+    type: String,
+    requred: true, //validating content field
+  },
+});
 
 const Notes = mongoose.model("Note", NotesSchema);
 module.exports = Notes;
