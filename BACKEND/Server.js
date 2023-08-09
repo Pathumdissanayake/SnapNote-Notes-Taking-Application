e214a5025fd3fddc2a1de8920a286f979dff1f1d
IT21258480 <100502665+Pathumdissanayake@users.noreply.github.com>
2023-08-09 10:31:37 +0530

    backend server.js file completed

--- BACKEND/Server.js
+++ BACKEND/Server.js
@@ -1,30 +1,30 @@
-const express = require ('express');
-const mongoose = require ('mongoose');
-const bodyParser = require ('body-parser');
-const cors = require ('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const App = express();
-require ("dotenv").config();
+require("dotenv").config();
 
 const PORT = process.env.PORT || 4000;
 
 App.use(cors());
 App.use(bodyParser.json());
 
-const NotesRouter = require ("./ROUTES/NotesRoute.js");
+const NotesRouter = require("./ROUTES/NotesRoute.js");
 
 //http://localhost:4000/Notes
-App.use("/Notes",NotesRouter);
+App.use("/Notes", NotesRouter);
 
 //connect to mongo db
 const URL = process.env.MONGODB_URL;
-mongoose.connect(URL)
-.then(() => {
-
+mongoose
+  .connect(URL)
+  .then(() => {
     // listen to port
     App.listen(PORT, () => {
-      console.log(`Connected to db & listening for requests on port ${PORT}`)
-    })
+      console.log(`Connected to db & listening for requests on port ${PORT}`);
+    });
   })
   .catch((err) => {
-    console.log(err)
-  }) 
+    console.log(err);
+  });
