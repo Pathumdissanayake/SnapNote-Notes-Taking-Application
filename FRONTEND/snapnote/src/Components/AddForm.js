f1dce4c1db92ed111d35d15e4e4f69d26797745b
IT21258480 <100502665+Pathumdissanayake@users.noreply.github.com>
2023-08-09 10:59:06 +0530

    frontend and backend connected
    create is done

--- FRONTEND/snapnote/src/Components/AddForm.js
+++ FRONTEND/snapnote/src/Components/AddForm.js
@@ -1,21 +1,71 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
 
 export default function AddForm() {
+  const [title, setTitle] = useState(""); // State for title input
+  const [content, setContent] = useState(""); // State for content textarea
+
+  const ValidateForm = () => {
+    let isValid = true;
+
+    if (!title) {
+      isValid = false;
+      alert("Enter the title for your note!");
+    } else if (!content) {
+      isValid = false;
+      alert("Enter Content to create a note!!");
+    }
+    return isValid;
+  };
+
+  const AddNewNote = (e) => {
+    e.preventDefault();
+
+    if (ValidateForm()) {
+      const newNoteData = {
+        title,
+        content,
+      };
+
+      axios
+        .post("http://localhost:4000/Notes/newNote", newNoteData)
+        .then(() => {
+          alert("New Note Created!");
+          window.location.reload();
+        })
+        .catch((err) => {
+          alert(err);
+        });
+    }
+  };
+
   return (
-    <div className='add-form-body'>
-        <h1>New Note</h1>
-        <form>
-            <input type="text" placeholder='Title'></input>
+    <div className="add-form-body">
+      <h1>New Note</h1>
+      <form onSubmit={AddNewNote}>
+        <input
+          type="text"
+          placeholder="Title"
+          name="title"
+          value={title} // Bind input value to state
+          onChange={(e) => setTitle(e.target.value)} // Update state on change
+        />
 
-            <br></br>
-            <textarea placeholder='Start writing here'/>
+        <br />
 
-            <br></br>
-            <button>Create</button>
-            <button>Cancel</button>
-            <br></br>
-            <button>+ Set Reminder</button>
-        </form>
+        <textarea
+          placeholder="Start writing here"
+          name="content"
+          value={content} // Bind textarea value to state
+          onChange={(e) => setContent(e.target.value)} // Update state on change
+        />
+
+        <br />
+        <button type="submit">Create</button>
+        <button type="button">Cancel</button>
+        <br />
+        <button type="button">+ Set Reminder</button>
+      </form>
     </div>
-  )
+  );
 }
