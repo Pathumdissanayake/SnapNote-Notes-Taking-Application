0e500d2c9d7779240afb6920e877a17c361a08c1
IT21258480 <100502665+Pathumdissanayake@users.noreply.github.com>
2023-08-09 13:34:00 +0530

    Update HomePage.js

--- FRONTEND/snapnote/src/Components/HomePage.js
+++ FRONTEND/snapnote/src/Components/HomePage.js
@@ -1,13 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import '../Styles/HomePage.css';
 
 export default function HomePage() {
+
+  const [notes, setNotes] = useState([]);
+
+  //read
+  useEffect(() => {
+    function getNotes() {
+      //jwt token
+      axios.get("http://localhost:4000/Notes/notes").then((res) => {
+        setNotes(res.data);
+      }).catch((err) => {
+        alert(err.message);
+      });
+    }
+    getNotes();
+  },[]);
+
+
   return (
     <div className='home-body'>
+      <div className='div1'>
+      <table>
+  <tbody>
+    {notes.map((n) => (
+      <tr key={n.id}>
+        <th className='title-column'>{n.title}</th>
+        <td className='content-column'>{n.content}</td>
+      </tr>
+    ))}
+  </tbody>
+</table>
+
+      </div>
+      <div className='div2'>
+
+      </div>
+      <div className='add-new-btn-div'>
       <Link to="/New-note" className='add-new-notes-btn'>
         <button>+</button>
       </Link>
+      </div>
     </div>
   );
 }
