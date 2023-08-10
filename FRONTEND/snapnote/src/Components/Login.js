72c4e602642337e10e2c937f99583f5fa1d582f7
IT21258480 <100502665+Pathumdissanayake@users.noreply.github.com>
2023-08-10 09:04:56 +0530

    Update Login.js

--- FRONTEND/snapnote/src/Components/Login.js
+++ FRONTEND/snapnote/src/Components/Login.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import "../Styles/Login.css";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -11,36 +12,60 @@ const Login = () => {
   const handleLoginFormSubmit = (e) => {
     e.preventDefault();
 
-    axios.post("http://localhost:4000/User/login", { email, password })
+    axios
+      .post("http://localhost:4000/User/login", { email, password })
       .then((result) => {
         if (result.data.message === "Success") {
           navigate("/home");
           alert("Login Successfully!!");
-        } else if(result.data.error === "Incorrect"){
-          alert("Register first!!")
+        } else if (result.data.error === "Incorrect") {
+          alert("Register first!!");
         }
       });
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleLoginFormSubmit}> {/* Move onSubmit to the form element */}
-        <div>
-          <label>Email:</label>
-          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
-        </div>
-        <div>
-          <label>Password:</label>
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
-        </div>
-        <div>
-          <button type="submit">Login</button>
-          <Link to="/register">
-            <button type="button">Register</button>
-          </Link>
-        </div>
-      </form>
+    <div className="login-body">
+      <div className="div-for-login-box">
+      <div className="login">
+      <div className="login-heading-div">
+        <h2>Login</h2>
+      </div>
+
+      <div className="login-form-container-div">
+        <form onSubmit={handleLoginFormSubmit}>
+          <div>
+            <label className="lable-login">Email:</label>
+            <input
+              className="login-input-field"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <label className="lable-login">Password :</label>
+            <input
+              className="login-input-field"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="btns-div">
+            <button type="submit" className="login-btn">
+              Login
+            </button>
+            <Link to="/register">
+              <button type="button" className="register-btn">
+                Register
+              </button>
+            </Link>
+          </div>
+        </form>
+      </div>
+      </div>
+      </div>
     </div>
   );
 };
