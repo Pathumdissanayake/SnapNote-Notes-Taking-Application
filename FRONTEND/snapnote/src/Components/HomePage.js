3044341b2987e56d0cfa2646b4e4bb91d5b20a24
IT21258480 <100502665+Pathumdissanayake@users.noreply.github.com>
2023-08-11 12:13:59 +0530

    Update HomePage.js

--- FRONTEND/snapnote/src/Components/HomePage.js
+++ FRONTEND/snapnote/src/Components/HomePage.js
@@ -6,7 +6,7 @@ import "../Styles/HomePage.css";
 export default function HomePage() {
   const [notes, setNotes] = useState([]);
   const [selectedNote, setSelectedNote] = useState(null);
-  const [isEditing, setIsEditing] = useState(false); // Add this state to control editing mode
+  const [isEditing, setIsEditing] = useState(false);
 
   useEffect(() => {
     async function getNotes() {
@@ -21,29 +21,15 @@ export default function HomePage() {
   }, []);
 
   const handleDelete = async (noteId) => {
-    const shouldDelete = window.confirm(
-      "Are you sure you want to delete this note?"
-    );
+    const shouldDelete = window.confirm("Are you sure you want to delete this note?");
     if (!shouldDelete) {
       return;
     }
 
     try {
-      const response = await axios.delete(
-        `http://localhost:4000/Notes/delete/${noteId}`
-      );
-      if (response.status === 200) {
-        setNotes((prevNotes) =>
-          prevNotes.filter((note) => note._id !== noteId)
-        );
-        setSelectedNote(null);
-        const updatedNotes = await axios.get(
-          "http://localhost:4000/Notes/notes"
-        );
-        setNotes(updatedNotes.data);
-      } else {
-        console.log("Note not deleted:", response.data);
-      }
+      await axios.delete(`http://localhost:4000/Notes/delete/${noteId}`);
+      setNotes((prevNotes) => prevNotes.filter((note) => note._id !== noteId));
+      setSelectedNote(null);
     } catch (error) {
       console.log("Error deleting note:", error);
       alert(`Error deleting note: ${error.message}`);
@@ -52,37 +38,25 @@ export default function HomePage() {
 
   const handleEdit = (noteId) => {
     setSelectedNote(notes.find((note) => note._id === noteId));
-    setIsEditing(true); // Set the editing state to true
+    setIsEditing(true);
   };
 
   const handleEditCancel = () => {
-    setIsEditing(false); // Set the editing state to false
+    setIsEditing(false);
   };
 
   const handleSave = async (updatedNote) => {
     try {
-      const response = await axios.put(
-        `http://localhost:4000/Notes/edit/${updatedNote._id}`,
-        {
-          title: updatedNote.title,
-          content: updatedNote.content,
-        }
-      );
-
-      if (response.status === 200) {
-        setIsEditing(false); // Exit edit mode
-        const updatedNotes = await axios.get(
-          "http://localhost:4000/Notes/notes"
-        );
-        setNotes(updatedNotes.data);
-      } else {
-        console.log("Note not updated:", response.data);
-      }
+      await axios.put(`http://localhost:4000/Notes/edit/${updatedNote._id}`, updatedNote);
+      setIsEditing(false);
+      const updatedNotes = await axios.get("http://localhost:4000/Notes/notes");
+      setNotes(updatedNotes.data);
     } catch (error) {
       console.log("Error updating note:", error);
       alert(`Error updating note: ${error.message}`);
     }
   };
+
   return (
     <div className="home-body">
       <div className="header-div">
@@ -113,57 +87,64 @@ export default function HomePage() {
         </div>
 
         <div className="div2">
-  {!selectedNote ? (
-    <div className="no-note-selected">
-      <p>View your notes here.</p>
-    </div>
-  ) : (
-    <div className="card2">
-      <div className="icon-and-title-div">
-        <div className="title-div">
-          {!isEditing ? (
-            <h3 className="title-column-display">{selectedNote.title}</h3>
-          ) : (
-            <input
-              type="text"
-              value={selectedNote.title}
-              onChange={(e) =>
-                setSelectedNote((prevNote) => ({
-                  ...prevNote,
-                  title: e.target.value,
-                }))
-              }
-            />
+          {selectedNote && (
+            <div className="card2">
+              <div className="icon-and-title-div">
+                <div className="title-div">
+                  {!isEditing ? (
+                    <h3 className="title-column-display">{selectedNote.title}</h3>
+                  ) : (
+                    <input
+                      type="text"
+                      value={selectedNote.title}
+                      onChange={(e) =>
+                        setSelectedNote((prevNote) => ({
+                          ...prevNote,
+                          title: e.target.value,
+                        }))
+                      }
+                    />
+                  )}
+                </div>
+                <div className="icon-div">
+                  {isEditing ? (
+                    <>
+                      <span className="icon save-icon" onClick={() => handleSave(selectedNote)}>
+                        💾
+                      </span>
+                      <span className="icon cancel-icon" onClick={handleEditCancel}>
+                        ❌
+                      </span>
+                    </>
+                  ) : (
+                    <>
+                      <span className="icon delete-icon" onClick={() => handleDelete(selectedNote._id)}>
+                        🗑️
+                      </span>
+                      <span className="icon edit-icon" onClick={() => handleEdit(selectedNote._id)}>
+                        ✏️
+                      </span>
+                    </>
+                  )}
+                </div>
+              </div>
+              {isEditing ? (
+                <textarea
+                  className="content-textarea"
+                  value={selectedNote.content}
+                  onChange={(e) =>
+                    setSelectedNote((prevNote) => ({
+                      ...prevNote,
+                      content: e.target.value,
+                    }))
+                  }
+                />
+              ) : (
+                <p className="content-column">{selectedNote.content}</p>
+              )}
+            </div>
           )}
         </div>
-
-        <div className="icon-div">
-          {isEditing ? (
-            <>
-              <span className="icon save-icon" onClick={() => handleSave(selectedNote)}>
-                💾
-              </span>
-              <span className="icon cancel-icon" onClick={handleEditCancel}>
-                ❌
-              </span>
-            </>
-          ) : (
-            <>
-              <span className="icon delete-icon" onClick={() => handleDelete(selectedNote._id)}>
-                🗑️
-              </span>
-              <span className="icon edit-icon" onClick={() => handleEdit(selectedNote._id)}>
-                ✏️
-              </span>
-            </>
-          )}
-        </div>
-      </div>
-      <p className="content-column">{selectedNote.content}</p>
-    </div>
-  )}
-</div>
-
       </div>
 
       <div className="add-new-btn-div">
